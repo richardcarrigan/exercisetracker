5625209bc6a9f3250edcc7cccbcfe9b7a17cc3a0
Richard Carrigan <nihonjinboy85@gmail.com>
2020-10-26 22:15:01 -0700

    Wire up GET log route

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/exercise/log', (req, res) => {
+  res.json({ 
+    msg: 'success'
+  });
+});
+
 app.post('/api/exercise/new-user', (req, res) => {
   console.log(req.body.username);
   res.json({ msg: 'success', username: req.body.username });
