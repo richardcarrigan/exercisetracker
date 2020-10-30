01a06d02edd21fb3060249722bf41d7d20b10175
Richard Carrigan <nihonjinboy85@gmail.com>
2020-10-29 21:24:58 -0700

    Clean up code, fulfill user story #4

--- server.js
+++ server.js
@@ -43,12 +43,13 @@ app.get('/api/exercise/users', async (req, res) => {
 app.get('/api/exercise/log', (req, res) => {
   const queryObject = url.parse(req.url, true).query;
   if(queryObject.userId) {
-    res.json({ 
-      msg: 'success',
-      userId: queryObject.userId,
-      from: queryObject.from,
-      to: queryObject.to,
-      limit: queryObject.limit
+    collection.findOne({ _id: new ObjectId(queryObject.userId) }).then(result => {
+      if(result !== null) {
+        const count = result.exercises.length;
+        res.json({...result, count: count });
+      } else {
+        res.json({ msg: `user ${queryObject.userId} not found` });
+      }
     });
   } else {
     res.json({ msg: 'failure', err: 'must include userId' });
@@ -61,10 +62,12 @@ app.post('/api/exercise/new-user', (req, res) => {
     if(user !== null) {
       res.json({ username: user.username, _id: user._id });
     } else {
-      collection.insertOne({ username: req.body.username, exercises: [] }).then(result => {
-        collection.findOne(query).then(user => {
-          res.json({ username: user.username, _id: user._id });
-        }).catch( err => res.json({ msg: err }));
+      collection.findOneAndUpdate(
+        query, 
+        { $set: { username: req.body.username, exercises: [] } },
+        { returnOriginal: false, upsert: true}
+      ).then(user => {
+        res.json({ username: user.value.username, _id: user.value._id });
       }).catch( err => res.json({ msg: err }));
     }
   }).catch( err => res.json({ msg: err }));
@@ -75,7 +78,7 @@ app.post('/api/exercise/add', (req, res) => {
   const exercise = {
     description: req.body.description,
     duration: req.body.duration,
-    date: req.body.date
+    date: req.body.date ? new Date(req.body.date) : new Date()
   };
 
   collection.findOneAndUpdate(filter, { $push: { exercises: exercise } }, { returnOriginal: false }).then(user => {
